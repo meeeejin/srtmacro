a20007c7434ffde630b3b3d157651e5f93353111
Mijin Ahn <meeeeejin@gmail.com>
2018-12-28 12:34:05 +0900

    Uploaded SRT Macro v1.0.4

--- background.js
+++ background.js
@@ -11,28 +11,9 @@ function playSound() {
 	audio.play();
 }
 
-function sendTelegramMessage() {
-	var botToken = localStorage['botToken'];
-	var chatId = localStorage['chatId'];
-	var msg = encodeURI('Macro has been stopped. Please check your reservation status.');
-	if (botToken != undefined && chatId != undefined) {
-		var url = 'https://api.telegram.org/bot' + botToken + '/sendmessage?chat_id=' + chatId + '&text=' + msg;
-		
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange=function() {
-			if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-				var response = xmlhttp.responseText; //if you need to do something with the returned value
-			}
-		}
-		xmlhttp.open('GET', url, true);
-		xmlhttp.send();
-	}
-}
-
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
     if (message && message.type == 'playSound') {
         playSound();
-		sendTelegramMessage();
         sendResponse(true);
     }
 });
--- content.js
+++ content.js
@@ -1,27 +1,69 @@
-function injectJs(srcFile) {
-    var scr = document.createElement('script');
-    scr.src = srcFile;
-    document.getElementsByTagName('head')[0].appendChild(scr);
+var dsturl1 = "https://etk.srail.co.kr/hpg/hra/01/selectScheduleList.do?pageId=TK0101010000";
+
+window.showModalDialog = window.showModalDialog || function(url, arg, opt) {
+	window.open(url, arg, opt);
+};
+
+function macrostart() {
+	coachSelected = [].map.call(document.querySelectorAll('.coachMacro:checked'), function (select) {
+		return select.value;
+	});
+	firstSelected = [].map.call(document.querySelectorAll('.firstMacro:checked'), function (select) {
+		return select.value;
+	});
+
+	if (coachSelected.length == 0 && firstSelected.length == 0) {
+		alert("매크로를 실행하기 위해서는 예매하기 위한 열차 1개 이상을 선택하십시오.");
+	} else {
+		alert("매크로를 시작합니다.\n트럼펫 소리가 나면 바로 결제를 해주셔야 합니다.");
+
+		sessionStorage.setItem('macro', true);
+		sessionStorage.setItem('coachSelected', JSON.stringify(coachSelected));
+		sessionStorage.setItem('firstSelected', JSON.stringify(firstSelected));
+
+		// Stores user preferences.
+		sessionStorage.setItem('psgInfoPerPrnb1', document.getElementsByName('psgInfoPerPrnb1')[0].value);
+		sessionStorage.setItem('psgInfoPerPrnb5', document.getElementsByName('psgInfoPerPrnb5')[0].value);
+		sessionStorage.setItem('psgInfoPerPrnb4', document.getElementsByName('psgInfoPerPrnb4')[0].value);
+		sessionStorage.setItem('psgInfoPerPrnb2', document.getElementsByName('psgInfoPerPrnb2')[0].value);
+		sessionStorage.setItem('psgInfoPerPrnb3', document.getElementsByName('psgInfoPerPrnb3')[0].value);
+		sessionStorage.setItem('locSeatAttCd1', document.getElementsByName('locSeatAttCd1')[0].value);
+		sessionStorage.setItem('rqSeatAttCd1', document.getElementsByName('rqSeatAttCd1')[0].value);
+
+		location.reload();
+	}
 }
 
-var dsturl1 = "https://etk.srail.co.kr/hpg/hra/01/selectScheduleList.do?pageId=TK0101010000";
+function macrostop() {
+	alert("매크로를 중지합니다.\n조건을 변경하여 재조회하신 후 다시 시작하실 수 있습니다.");
+
+	sessionStorage.removeItem('macro');
+	sessionStorage.removeItem('coachSelected');
+	sessionStorage.removeItem('firstSelected');
+	sessionStorage.removeItem('psgInfoPerPrnb1');
+	sessionStorage.removeItem('psgInfoPerPrnb5');
+	sessionStorage.removeItem('psgInfoPerPrnb4');
+	sessionStorage.removeItem('psgInfoPerPrnb2');
+	sessionStorage.removeItem('psgInfoPerPrnb3');
+	sessionStorage.removeItem('locSeatAttCd1');
+	sessionStorage.removeItem('rqSeatAttCd1');
+
+	location.reload();
+}
 
 if (document.URL.substring(0, dsturl1.length) == dsturl1) {
 
 	$(document).ready(function() {
-		injectJs(chrome.extension.getURL('inject.js'));
 
 		var coachSelected = JSON.parse(sessionStorage.getItem('coachSelected'));
 		var firstSelected = JSON.parse(sessionStorage.getItem('firstSelected'));
 		if (coachSelected == null) coachSelected = [];
 		if (firstSelected == null) firstSelected = [];
-		console.log("coach:" + coachSelected);
-		console.log("first:" + firstSelected);
 
 		if (sessionStorage.getItem('macro') == "true") {
-			$("div.button").append('<a href="#" onclick="macrostop();" style="margin-left:5px;"><img src="' + chrome.extension.getURL('images/btn_stop.png') + '"></a>');
+			$("div#search_top_tag.tal_c.mgt30").append('<a href="#" onclick="macrostop();" style="margin-left:5px;display:inline-block;height:100%;vertical-align:middle;"><img src="' + chrome.runtime.getURL('images/btn_stop.png') + '"></a>');
 		} else {
-			$("div.button").append('<a href="#" onclick="macro();" style="margin-left:5px;"><img src="' + chrome.extension.getURL('images/btn_start.png') + '"></a>');
+			$("div#search_top_tag.tal_c.mgt30").append('<a href="#" onclick="macrostart();" style="margin-left:5px;display:inline-block;height:100%;vertical-align:middle;"><img src="' + chrome.runtime.getURL('images/btn_start.png') + '"></a>');
 		}
 
 		$("<style>")
@@ -118,7 +160,7 @@ if (document.URL.substring(0, dsturl1.length) == dsturl1) {
 					sessionStorage.removeItem('rqSeatAttCd1');
 					chrome.extension.sendMessage({type: 'playSound'}, function(data) { });
 				} else {
-					setTimeout(function() { 
+					setTimeout(function() {
 					location.reload();
 					}, 1000);
 				}
